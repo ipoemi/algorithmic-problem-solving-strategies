51ec484ac3e1b8377bcb836477b8e5e3dd8cf4fe
ipoemi <ipoemi@nate.com>
2016-12-07 17:42:19 +0900

    LIS Problem Solved

---
+++ src/main/scala/ch08/P4LIS.scala
@@ -0,0 +1,67 @@
+package ch08
+
+import scala.collection.mutable
+
+/**
+	* Created by ipoemi on 2016-12-05.
+	*/
+object P4LIS {
+
+	import scala.io._
+
+	val in: String =
+		"""3
+			|4
+			|1 2 3 4
+			|8
+			|5 4 3 2 1 6 7 8
+			|8
+			|5 6 7 8 1 2 3 4
+			|""".stripMargin
+
+	/*
+	def solve(seq: Seq[Int], cache: mutable.HashMap[Seq[Int], Seq[Int]] = mutable.HashMap()): Seq[Int] = {
+		if (seq.isEmpty) return seq
+		cache.getOrElseUpdate(seq, {
+			seq.indices.map { n =>
+				seq(n) +: solve(seq.slice(n, seq.size).filter(_ > seq(n)), cache)
+			}.maxBy(_.size)
+		})
+	}
+	*/
+
+	def solve(seq: Seq[Int]): Int = {
+		if (seq.isEmpty) return 0
+
+		val cache: Array[Option[Int]] = Array.fill(seq.size + 1)(None)
+
+		def aux(idx: Int): Int = {
+			val cachedValue = cache(idx + 1)
+			if (cachedValue.nonEmpty) return cachedValue.get
+			val retList =
+				for (i <- seq.indices if i > idx && (idx == -1 || seq(i) > seq(idx))) yield
+					1 + aux(i)
+			cache(idx + 1) = Some((retList :+ 1).max)
+			cache(idx + 1).get
+		}
+
+		Seq(0 to 100).find(_ == 2)
+
+		aux(-1) - 1
+	}
+
+	def main(args: Array[String]): Unit = {
+		val source = Source.fromString(in).getLines()
+		val testCount = source.next().toInt
+		(1 to testCount).foreach {
+			testNo =>
+				val _ = source.next()
+				val sequence = source.next().split(" ").toVector.map(_.toInt)
+
+				println(s"-- testCase $testNo --")
+				println(s"Sequence: $sequence")
+				println(solve(sequence))
+		}
+	}
+
+}

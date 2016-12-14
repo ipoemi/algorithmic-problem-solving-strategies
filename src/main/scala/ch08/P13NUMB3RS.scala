a30246fca3fb7b74768c22be0fe176a8a822b658
ipoemi <ipoemi@nate.com>
2016-12-14 12:49:33 +0900

    NUMB3RS Problem Solved

---
+++ src/main/scala/ch08/P13NUMB3RS.scala
@@ -0,0 +1,72 @@
+package ch08
+
+import scala.collection.mutable
+
+/**
+	* Created by ipoemi on 2016-12-05.
+	*/
+object P13NUMB3RS {
+
+	import scala.io._
+
+	val in: String =
+		"""2
+			|5 2 0
+			|0 1 1 1 0
+			|1 0 0 0 1
+			|1 0 0 0 0
+			|1 0 0 0 0
+			|0 1 0 0 0
+			|3
+			|0 2 4
+			|8 2 3
+			|0 1 1 1 0 0 0 0
+			|1 0 0 1 0 0 0 0
+			|1 0 0 1 0 0 0 0
+			|1 1 1 0 1 1 0 0
+			|0 0 0 1 0 0 1 1
+			|0 0 0 1 0 0 0 1
+			|0 0 0 0 1 0 0 0
+			|0 0 0 0 1 1 0 0
+			|4
+			|3 1 2 6
+			|""".stripMargin
+
+	def solve(matrix: Seq[Seq[Int]], dayCnt: Int, startNo: Int, targetNoList: Vector[Int]): String = {
+		val cache: mutable.HashMap[(Int, Int), Double] = mutable.HashMap()
+		val deg = matrix.indices.map { no =>
+			matrix.indices.count(n => matrix(no)(n) == 1)
+		}.toVector
+
+		def aux(villageNo: Int, days: Int): Double = {
+			if (days == 0) return if (villageNo == startNo) 1 else 0
+			cache.getOrElseUpdate((villageNo, days), {
+				val preVillages = matrix.indices.filter(n => matrix(villageNo)(n) == 1)
+				preVillages.map { prev =>
+					1.0 / deg(prev) * aux(prev, days - 1)
+				}.sum
+			})
+		}
+
+		targetNoList.map(no => aux(no, dayCnt).formatted("%.7f")).mkString(" ")
+	}
+
+	def main(args: Array[String]): Unit = {
+		val source = Source.fromString(in).getLines()
+		val testCount = source.next().toInt
+		(1 to testCount).foreach { testNo =>
+			val Array(villageCnt, dayCnt, startNo) = source.next().split(" ").map(_.toInt)
+			val matrix = (0 until villageCnt).map(_ => source.next().split(" ").map(_.toInt).toVector).toVector
+			val _ = source.next()
+			val targetNoList = source.next().split(" ").map(_.toInt).toVector
+
+			println(s"-- testCase $testNo --")
+			//println(s"VillageCnt: $villageCnt")
+			//println(s"DayCnt: $dayCnt")
+			//println(s"StartNo: $startNo")
+			//println(s"TargetNoList: $targetNoList")
+			println(solve(matrix, dayCnt, startNo, targetNoList))
+		}
+	}
+
+}

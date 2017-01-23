e8f19b139cdc04eba4be54ca60a960e87d3cc3ec
ipoemi <ipoemi@nate.com>
2017-01-24 00:09:46 +0900

    Allergy Problem Is Solving

--- src/main/scala/ch11/P02Allergy.scala
+++ src/main/scala/ch11/P02Allergy.scala
@@ -28,7 +28,7 @@ object P02Allergy {
   def main(args: Array[String]): Unit = {
     val source = Source.fromString(in).getLines()
     val testCount = source.next().toInt
-    val startTime = System.currentTimeMillis()
+    val startTime = System.nanoTime()
     (1 to testCount).foreach { testNo =>
       val Array(_, foodCnt) = source.next().split(" ").map(_.toInt)
       val friends = source.next().split(" ").toSeq
@@ -40,7 +40,7 @@ object P02Allergy {
       foods.foreach(println)
       println(solve(friends, foods))
     }
-    println(s"Spent Time: ${System.currentTimeMillis() - startTime}")
+    println(s"Spent Time: ${(System.nanoTime() - startTime) / 1000.0}")
   }
 
   def solve(friends: Seq[String], foods: Seq[Seq[Int]]): String = {
@@ -55,14 +55,53 @@ object P02Allergy {
             if (selectedFood.size < best.size) return selectedFood
           best
         case _ =>
-          val value = aux(remainFoods.tail, selectedFood, best)
-          aux(remainFoods.tail, selectedFood :+ remainFoods.head, value)
+          if (selectedFood.size < best.size) {
+            val value = aux(remainFoods.tail, selectedFood, best)
+            aux(remainFoods.tail, selectedFood :+ remainFoods.head, value)
+          } else {
+            best
+          }
       }
     }
 
     val result = aux(foods.indices, Seq(), foods.indices)
     println(result)
     result.size.toString
+  }
+
+  def solve2(friends: Seq[String], foods: Seq[Seq[Int]]): String = {
+
+    val canEat = friends.indices.map(friendNo => foods.indices.filter(idx => foods(idx).contains(friendNo)))
+
+    def aux(edible: Seq[Boolean], chosen: Int, best: Int): Int = {
+      if (chosen >= best) {
+        best
+      } else {
+        val first = edible.indexWhere(!_)
+        if (first == -1) {
+          chosen
+        } else  {
+          canEat(first).foldLeft(best) { (newBest, foodNo) =>
+            val newEdible = foods(foodNo).foldLeft(edible) { (acc, friendNo) => acc.updated(friendNo, true)}
+            newBest.min(aux(newEdible, chosen + 1, newBest))
+          }
+        }
+      }
+
+      /*
+      if (foodNo == foods.size - 1) {
+        if (edible.forall(identity)) chosen
+        else best
+      } else {
+        val newBest = aux(foodNo + 1, edible, chosen, best)
+        val newEdible = foods(foodNo).foldLeft(edible) { (acc, friendNo) => acc.updated(friendNo, true)}
+        aux(foodNo + 1, newEdible, chosen + 1, newBest)
+      }
+      */
+    }
+
+    //aux(0, friends.map(_ => false), 0, foods.size).toString
+    aux(friends.map(_ => false), 0, foods.size).toString
   }
 
 }
